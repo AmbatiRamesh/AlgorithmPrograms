ca19b86acb1756838eb025bf54403c9dec720376
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-03-02 12:52:37 +0530

    Added:"Programs"

---
+++ InsertionSort.java
@@ -0,0 +1,25 @@
+package com.programs;
+import java.util.Arrays;
+
+public class InsertionSort {
+    void insertionSort(int array[]) {
+        int size = array.length;
+        for (int i = 1; i < size; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while (j >= 0 && key < array[j]) {
+                array[j + 1] = array[j];
+                --j;
+            }
+            array[j + 1] = key;
+        }
+    }
+    public static void main(String args[]) {
+        int[] data = { 45, 17, 7, 49, 18 };
+        InsertionSort sort = new InsertionSort();
+        sort.insertionSort(data);
+        System.out.println("After Insertion Sort");
+        System.out.println(Arrays.toString(data));
+    }
+}
+
